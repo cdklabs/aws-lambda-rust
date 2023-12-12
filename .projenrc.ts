9fd30425daec06e2b70b1ca20c002b1db19ea519
cdklabs-automation <90142015+cdklabs-automation@users.noreply.github.com>
2023-12-11 16:09:09 -0800

    chore(deps): upgrade dev dependencies (#2)
    * chore(deps): upgrade dev dependencies
    
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdklabs/aws-lambda-rust/actions/runs/7174938239
    
    ------
    
    *Automatically created by projen via the "upgrade-dev-deps" workflow*
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,14 @@
-import { CdklabsConstructLibrary } from "cdklabs-projen-project-types";
+import { CdklabsConstructLibrary } from 'cdklabs-projen-project-types';
 const project = new CdklabsConstructLibrary({
-  author: "AWS",
-  authorAddress: "aws-cdk-dev@amazon.com",
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  devDeps: ["cdklabs-projen-project-types"],
-  name: "aws-lambda-rust",
+  author: 'AWS',
+  authorAddress: 'aws-cdk-dev@amazon.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  devDeps: ['cdklabs-projen-project-types'],
+  name: 'aws-lambda-rust',
   projenrcTs: true,
   release: false,
-  repositoryUrl: "https://github.com/cdklabs/aws-lambda-rust.git",
+  repositoryUrl: 'https://github.com/cdklabs/aws-lambda-rust.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
